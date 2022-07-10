cecfa31b38b39d40501fe9f4afbe4a1b96cf15c0
Milan Felix Šulc <rkfelix@gmail.com>
2022-07-10 20:29:25 +0200

    App: make /app/var writable

--- entrypoint-app.sh
+++ entrypoint-app.sh
@@ -5,6 +5,7 @@ bin/console d:m:m --no-interaction
 bin/console messenger:setup-transports --no-interaction
 bin/console repman:security:update-db
 bin/console assets:install
+chmod 0777 /app/var
 
 php-fpm &
 nginx
