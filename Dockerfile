bc74a2822d39018f8c6c6e4cb5c32cc6c05a1e05
Milan Felix Šulc <rkfelix@gmail.com>
2022-07-10 18:51:00 +0200

    App: allow use S3 style path

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,11 @@ FROM buddy/repman:1.4.0
 
 RUN apk add nginx tini --no-cache
 
-ADD ./nginx/nginx.conf /etc/nginx/nginx.conf
-ADD ./cron/crontabs /var/spool/cron/crontabs
+COPY ./nginx/nginx.conf /etc/nginx/nginx.conf
+COPY ./cron/crontabs /var/spool/cron/crontabs
+
+# S3 path style
+COPY ./patch/S3AdapterFactory.php /app/src/Service/Integration/Aws/S3AdapterFactory.php
 
 COPY ./entrypoint-* /
 
---
+++ patch/S3AdapterFactory.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Buddy\Repman\Service\Integration\Aws;
+
+use Aws\S3\S3Client;
+
+final class S3AdapterFactory
+{
+    private string $region;
+
+    private bool $isOpaqueAuth;
+
+    private string $key;
+
+    private string $secret;
+
+    private ?string $endpoint;
+
+    public function __construct(
+        string $region,
+        bool $isOpaqueAuth,
+        ?string $key = null,
+        ?string $secret = null,
+        ?string $endpoint = null
+    ) {
+        $this->region = $region;
+        $this->isOpaqueAuth = $isOpaqueAuth;
+        $this->endpoint = $endpoint;
+
+        if ($this->isOpaqueAuth) {
+            if ($key === null || $key === '') {
+                throw new \InvalidArgumentException('Must pass AWS key when authentication is opaque');
+            }
+            if ($secret === null || $secret === '') {
+                throw new \InvalidArgumentException('Must pass AWS secret when authentication is opaque');
+            }
+
+            $this->key = $key;
+            $this->secret = $secret;
+        }
+    }
+
+    public function create(): S3Client
+    {
+        $args = [
+            'region' => $this->region,
+            'version' => 'latest',
+        ];
+
+        if ($this->isOpaqueAuth) {
+            $args['credentials'] = [
+                'key' => $this->key,
+                'secret' => $this->secret,
+            ];
+        }
+
+        if ($this->endpoint !== null) {
+            $args['endpoint'] = $this->endpoint;
+            $args['use_path_style_endpoint'] = true;
+        }
+
+        return new S3Client($args);
+    }
+}
