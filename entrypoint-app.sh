9bd6397024c9c6a5526eb78f75af1fdc9766b140
Milan Felix Šulc <rkfelix@gmail.com>
2022-07-11 17:00:14 +0200

    App: chown www-data var/log var/cache var/repo

--- entrypoint-app.sh
+++ entrypoint-app.sh
@@ -5,7 +5,8 @@ bin/console d:m:m --no-interaction
 bin/console messenger:setup-transports --no-interaction
 bin/console repman:security:update-db
 bin/console assets:install
-chmod 0777 /app/var
+mkdir -p /app/var/{log,cache,repo}
+chown www-data:www-data /app/var /app/var/{log,cache,repo}
 
 php-fpm &
 nginx
